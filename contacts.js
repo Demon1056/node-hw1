06bcb59abc8b515759ff5249a399da8efa857b65
Demon1056 <102851685+Demon1056@users.noreply.github.com>
2023-01-12 00:32:22 +0200

    done first homework

--- contacts.js
+++ contacts.js
@@ -1,39 +1,60 @@
-const fs = require('fs').promises;
-const path = require('path')
-const contactsPath = path.resolve(__dirname, "db/contacts.json")
+const fs = require("fs").promises;
+const path = require("path");
+const shortid = require("shortid");
+const contactsPath = path.resolve(__dirname, "db/contacts.json");
 
 async function readFile() {
-    const contacts = await fs.readFile(contactsPath, "utf-8")
-    return contacts
+  const contacts = await fs.readFile(contactsPath, "utf-8");
+  return contacts;
 }
 
-async function writeFile(db) {
-    await fs.writeFile(dbPath, JSON.stringify(db, null, 2))
+async function writeFile(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 }
 
 async function listContacts() {
-    const contacts = await readFile()
-    const dataForTable = JSON.parse(contacts)
-    console.table(dataForTable);
-
+  const contacts = await readFile();
+  const dataForTable = JSON.parse(contacts);
+  console.table(dataForTable);
+  return dataForTable;
 }
 
 async function getContactById(contactId) {
-    const contacts = await readFile()
-    const contactsArray = JSON.parse(contacts)
-    console.log(contactsArray.filter(todo => contactId === todo.id));
+  const contacts = await readFile();
+  const contactsArray = JSON.parse(contacts);
+  const findContact = contactsArray.find(
+    (todo) => contactId.toString() === todo.id
+  );
+  console.log(findContact);
+  return findContact;
 }
 
-function removeContact(contactId) {
-    // ...твій код
+async function removeContact(contactId) {
+  const contacts = await readFile();
+  const contactsArray = JSON.parse(contacts);
+  const newContacts = contactsArray.filter(
+    (todo) => contactId.toString() !== todo.id
+  );
+  await writeFile(newContacts);
 }
+async function addContact(name, email, phone) {
+  id = shortid.generate();
+  const newContact = {
+    name,
+    email,
+    phone,
+    id,
+  };
+  const contacts = await readFile();
+  const contactsArray = JSON.parse(contacts);
+  contactsArray.push(newContact);
 
-function addContact(name, email, phone) {
-    // ...твій код
+  await writeFile(contactsArray);
+  console.log(contacts);
 }
 module.exports = {
-    readFile,
-    writeFile,
-    listContacts,
-    getContactById
-}
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const { readFile, writeFile, listContacts, getContactById } = require("./contacts")
+const argv = require("yargs").argv;
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
 
-async function main() { getContactById('2') }
-main()
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
 
+    case "get":
+      getContactById(id);
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+invokeAction(argv);
